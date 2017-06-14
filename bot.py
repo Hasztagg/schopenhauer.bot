36362107e122cbf61dede90a7e19665f107c9ac5
Adam <a.jurkiewicz5@gmail.com>
2017-06-14 09:56:42 +0200

    Fixed ball, coin toss

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import random
+import time
 from discord.ext.commands import Bot
 import vars
 
@@ -12,30 +13,30 @@ async def on_ready():
 	print(schopenhauer.user.id)
 	print("---------")
 
-@schopenhauer.command()
+@schopenhauer.command
 async def hello(*args):
 	print (args)
 	return await schopenhauer.say("Witam, i tak wszyscy umrzemy")
 
 """@schopenhauer.command()
-async def magicznakula(*args):
-  magiczne_powiedzenia = ["Tak", "Nie", "Nie wiem", "Hokus pokus, zapach lata, pora idejść z tego świata"]
-  await self.schopenhauer.send_message(message.channel.format(random.choice(magiczne_powiedzenia)))
 """
 
-@schopenhauer.command()
-async def rzut_moneta(*args):
-	wynik = random.randrange(0,3)
-	if wynik == 0:
-		wynik == "Orzeł. Nie żyje."
-	if wynik == 1:
-		wynik == "Reszka. I po co to komu."
-	if wynik == 2:
-		wynik == "Nie wiem skończyły mi się monety. Znowu."
-	schopenhauer.send_message(message.channel, "Czekaj no, szukam monety...")
-	time.sleep(1)
-	schopenhauer.send_message(message.channel, "%s" % wynik)
-
+@schopenhauer.event
+async def on_message(message):
+	if message.content.startswith('!rzut'):
+		wynik = random.randrange(0,3)
+		if wynik == 0:
+			wynik = "Orzeł. Nie żyje."
+		if wynik == 1:
+			wynik = "Reszka. I po co to komu."
+		if wynik == 2:
+			wynik = "Nie wiem skończyły mi się monety. Znowu."
+		await schopenhauer.send_message(message.channel, "Czekaj no, szukam monety...")
+		time.sleep(1)
+		await schopenhauer.send_message(message.channel, "%s" % wynik)
+	if message.content.startswith('!kula'):
+		magiczne_powiedzenia = ["Tak", "Nie", "Nie wiem", "Hokus pokus, zapach lata, pora idejść z tego świata."]
+		await schopenhauer.send_message(message.channel, (random.choice(magiczne_powiedzenia)))
 
 
 schopenhauer.run(vars.BOT_TOKEN)
